2f2458e5624e1d08e2991fd171560de61c499f31
Cristian Hampus <contact@cristianhampus.se>
2012-05-04 10:29:48 +0200

    Program 3.2

--- prog1/main.m
+++ prog1/main.m
@@ -1,15 +1,51 @@
 #import <Foundation/Foundation.h>
 
+@interface Fraction : NSObject
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+@end
+
+@implementation Fraction
+
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n
+{
+    numerator = n;
+}
+
+-(void) setDenominator: (int) d
+{
+    denominator = d;
+}
+
+@end
+
+
 int main(int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
-    int value1, value2, sum;
+    Fraction *myFraction;
     
-    value1 = 50;
-    value2 = 25;
-    sum = value1 + value2;
-    NSLog(@"The sum of %i and %i is %i", value1, value2, sum);
+    myFraction = [Fraction alloc];
+    myFraction = [myFraction init];
+    
+    [myFraction setNumerator: 1];
+    [myFraction setDenominator: 3];
+    
+    NSLog(@"The value of myFraction is:");
+    [myFraction print];
+    [myFraction release];
     
     [pool drain];
     return 0;
