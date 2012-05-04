0490c402a0b459fd96c399e9d73aa9e4ba439b3e
Cristian Hampus <contact@cristianhampus.se>
2012-05-04 09:59:51 +0200

    Program 2.1

--- prog1/main.m
+++ prog1/main.m
@@ -1,22 +1,11 @@
-//
-//  main.m
-//  prog1
-//
-//  Created by Cristian Hampus on 2012-05-04.
-//  Copyright (c) 2012 Queensbridge. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[])
 {
-
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
-    }
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSLog(@"Programming is fun!");
+    
+    [pool drain];
     return 0;
 }
 
