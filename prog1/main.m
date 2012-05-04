fd93528db9ce26b9991d85cc2aecbb07cccae0fa
Cristian Hampus <contact@cristianhampus.se>
2012-05-04 10:02:01 +0200

    Program 2.3

--- prog1/main.m
+++ prog1/main.m
@@ -3,8 +3,7 @@
 int main(int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    NSLog(@"Programming is fun!");
-    NSLog(@"Programming in Objective-C is even more fun!");
+    NSLog(@"Testing...\n..1\n...2\n....3");
     
     [pool drain];
     return 0;
