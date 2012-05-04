be0c000b666b871d21bef5979c36f0e98bb228aa
Cristian Hampus <contact@cristianhampus.se>
2012-05-04 09:46:37 +0200

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Cristian Hampus on 2012-05-04.
+//  Copyright (c) 2012 Queensbridge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
