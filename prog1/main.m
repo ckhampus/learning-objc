141fead64811b40056e2e6a307b34b60fe2e224f
Cristian Hampus <contact@cristianhampus.se>
2012-05-04 10:05:26 +0200

    Program 2.4

--- prog1/main.m
+++ prog1/main.m
@@ -3,7 +3,11 @@
 int main(int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    NSLog(@"Testing...\n..1\n...2\n....3");
+    
+    int sum;
+    
+    sum = 50 + 25;
+    NSLog(@"The sum of 50 and 25 is %i", sum);
     
     [pool drain];
     return 0;
